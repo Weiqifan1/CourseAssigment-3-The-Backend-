68176b3b1cca28b4da74240d633a8a5448bf473c
walter01git <26571249+walter01git@users.noreply.github.com>
2018-05-02 09:27:23 +0200

    Search bar på dummy data. Bemærk at det ikke virker i IE11, da metoden: includes ikke supportes i den browser.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,6 +13,87 @@ import UserHistory from './UserHistory';
 import Header from './Header'
 import Footer from './Footer'
 
+const dummydata = [
+  {
+  id: 1,
+  name: "Dominos",
+  type :  "fast food",
+  price_range: "cheap",
+  number_of_reviews : "00",
+  url : "dominos.dk"
+},
+{
+  id: 2,
+  name : "McDonalds",
+  type :  "fast food, family",
+  price_range : "cheap",
+  number_of_reviews : "00",
+  url : "www.mcdonalds.dk"
+},
+{
+  id: 3,
+  name : "Burger King",
+  type :  "fast food, family",
+  price_range : "cheap",
+  number_of_reviews : "00",
+  url: "www.burgerking.dk"
+},
+{
+  id: 4,
+  name : "Cafe Woody",
+  type :  "family, local",
+  price_range : "expensive",
+  number_of_reviews : "00",
+  url : "www.cafewoody.dk"
+},
+{
+  id: 5,
+  name : "Sticks'nSushi",
+  type :  "family, local, asian",
+  price_range : "expensive",
+  number_of_reviews : "00",
+  url : "www.sushi.dk"
+},
+{
+  id: 6,
+  name : "Big Mamma's Pizzaria",
+  type :  "fast food, family, local, pizzaria",
+  price_range : "average",
+  number_of_reviews : "00",
+  url : "www.bigmamma-pizzahouse.dk"
+},
+{
+  id: 7,
+  name : "Rådvad Kro",
+  type :  "luxus, family",
+  price_range : "expensive",
+  number_of_reviews : "00",
+  url : "www.raadvadkro.dk"
+},
+{
+  id: 8,
+  name : "Post-Pop Steakhouse",
+  type :  "local, luxus, steakhouse",
+  price_range : "expensive",
+ number_of_reviews : "00",
+  url : "www.postpub.dk/"
+},
+{
+  id: 9,
+ name : "Noma",
+  type :  "luxus, gourmet",
+  price_range : "very expensive",
+  number_of_reviews : "00",
+  url : "www.noma.dk"
+},
+]
+
+ function searchingFor(term){
+ return function(x){
+  return x.name.toLowerCase().includes(term.toLowerCase()) || !term;
+ }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -20,7 +101,18 @@ class App extends Component {
     this.state = {
       loggedIn: false,
     }
+
+    this.state = {
+      dummydata: dummydata ,
+      term: '',
+    }
+    this.searchHandler = this.searchHandler.bind(this);
   }
+
+  searchHandler(event){
+    this.setState({ term: event.target.value});
+  }
+  
 
   logout = () => {
     //TODO: Change url and go to home. The redirect does not work.
@@ -36,8 +128,32 @@ class App extends Component {
 
   render() {
 
+    const {term, dummydata} = this.state;
+
     return (
+      
       <div>
+
+      <div>
+        <form>
+          <input type="text" onChange={this.searchHandler}
+          value={term} />
+          
+          </form>
+        {
+          dummydata.filter(searchingFor(term)).map( cusines =>
+          
+          <div key={cusines.id}> 
+         <h2> {cusines.name} </h2>
+         <h2> {cusines.type} </h2>
+         <h2> {cusines.url} </h2>
+          </div>
+        )
+        }
+         </div>
+    );
+      }
+
 
         <HashRouter>
           <div>
