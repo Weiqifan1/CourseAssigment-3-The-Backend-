b1955ccf92ab5fbd7e53dc0d914ee9142dc26a79
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-05-04 22:01:21 +0200

    Merge origin/task42 into TesCI
    Conflicts:
    	jwtBackend/target/classes/entity/Role_.class
    	jwtBackend/target/classes/entity/User_.class

--- jwtBackend/src/main/java/security/GoogleAPI.java
+++ jwtBackend/src/main/java/security/GoogleAPI.java
@@ -13,19 +13,48 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.ws.rs.GET;
 /**
  *
  * @author Christian
  */
+@Path("googleplaces")
+
 public class GoogleAPI {
+    
+    @GET
+    @Path("/{place}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPlaceByText(@PathParam("place") String search) throws IOException {
+        String outPut = getplaceByTextSearch(search);
+        return outPut;
+}
 
     private static final String UrlFirstPart = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
     private static final String StandardRadius = "&radius=500";
     private static final String UrlKey = "AIzaSyBbdu5tPAp2P0EGbFgdGfzk_Vz7GUbsNO0";
     private static final String URLexample = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key=AIzaSyBbdu5tPAp2P0EGbFgdGfzk_Vz7GUbsNO0";
     private static final String USER_AGENT = "Mozilla/5.0";
-
+    private static final String TextSerarchUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json";
+    
+    
+    private static String getplaceByTextSearch(String search) throws IOException {
+        String trim = search.replace(" ", "+").trim();
+        System.out.println(trim);
+        
+        String requestUrl = TextSerarchUrl + "?query=" + trim + "&key=" + UrlKey;
+        System.out.println(requestUrl);
+        return sendGET(requestUrl);
+}
     private static String getGooglePlaceByCoor(String latAndLng, int radius) throws IOException {
         String mystr
                 = UrlFirstPart + "?location=" + latAndLng + radius + "&key=" + UrlKey;
@@ -37,31 +66,31 @@ public class GoogleAPI {
                 = UrlFirstPart + "?location=" + latAndLng + StandardRadius + "&key=" + UrlKey;
         return sendGET(mystr);
     }
-        
+
     private static String regexBetwTags(String text, String tag1, String tag2) {
-        final Pattern pattern = Pattern.compile(tag1+"(.+?)"+tag2);
+        final Pattern pattern = Pattern.compile(tag1 + "(.+?)" + tag2);
         final Matcher matcher = pattern.matcher(text);
         matcher.find();
         return matcher.group(1);
     }
-    
-    private static String getGoogleCityByCoor(String googleJSON){
+
+    private static String getGoogleCityByCoor(String googleJSON) {
         String firstWork = regexBetwTags(googleJSON, "name", ",");
-        String secondWork = firstWork.substring(3);  
+        String secondWork = firstWork.substring(3);
         secondWork = secondWork.trim();
-        secondWork = secondWork.substring(1,secondWork.length()-1);
+        secondWork = secondWork.substring(1, secondWork.length() - 1);
         return secondWork;
     }
-    
+
     private static String getGoogleStreetByCoor(String googleJSON) {
         String firstWork = regexBetwTags(googleJSON, "rating(.*)vicinity", "\"");
         firstWork = regexBetwTags(firstWork, "vicinity", "}");
-        String secondWork = firstWork.substring(3);   
+        String secondWork = firstWork.substring(3);
         secondWork = secondWork.trim();
-        secondWork = secondWork.substring(1,secondWork.length()-1);
+        secondWork = secondWork.substring(1, secondWork.length() - 1);
         return secondWork;
     }
-    
+
     private static String sendGET(String myUrl) throws IOException {
         String output = "";
         URL obj = new URL(myUrl);//GET_URL
@@ -90,10 +119,10 @@ public class GoogleAPI {
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(getGooglePlaceByCoor("-33.8670522,151.1957362"));
-        System.out.println(getGoogleCityByCoor(getGooglePlaceByCoor("-33.8670522,151.1957362")));
-        System.out.println(getGoogleStreetByCoor(getGooglePlaceByCoor("-33.8670522,151.1957362")));
-        
+//        System.out.println(getGooglePlaceByCoor("-33.8670522,151.1957362"));
+//        System.out.println(getGoogleCityByCoor(getGooglePlaceByCoor("-33.8670522,151.1957362")));
+//        System.out.println(getGoogleStreetByCoor(getGooglePlaceByCoor("-33.8670522,151.1957362")));
+ System.out.println(getplaceByTextSearch("mac donalds in copenhagen"));
     }
 
 }
