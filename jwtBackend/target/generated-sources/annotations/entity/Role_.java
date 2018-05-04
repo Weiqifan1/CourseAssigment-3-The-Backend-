e5d370241eeff8bcc5eadf00c9daff39dfd1cebf
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-05-04 15:35:06 +0200

    new find by category method i remoteendpoint

--- jwtBackend/target/generated-sources/annotations/entity/Role_.java
+++ jwtBackend/target/generated-sources/annotations/entity/Role_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-01T23:17:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-04T15:32:13")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
