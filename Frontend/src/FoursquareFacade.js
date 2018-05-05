4d51d23abb254042b16c4c56dc673e44f34258ec
Bo Henriksen <bomarconi@gmail.com>
2018-05-05 15:45:26 +0200

    Ryddet op i FoursquareFacade.

--- Frontend/src/FoursquareFacade.js
+++ Frontend/src/FoursquareFacade.js
@@ -1,43 +1,36 @@
-import React, { Component } from "react";
-import Logo_black from './images/Logo_black.jpg'
-import Powered_by_Foursquare_black_300 from './images/Powered_by_Foursquare_black_300.png'
-import facade from './ApiFacade'
-
-
-const URL = "https://benedikteeva.dk/jwtBackend%2D1.0%2DSNAPSHOT/api/restaurants/";
-
 
 function handleHttpErrors(res) {
-    if (!res.ok) {
+  if (!res.ok) {
+    throw { message: res.statusText, status: res.status };
+  }
 
-        throw { message: res.statusText, status: res.status };
-    }
-
-    return res.json();
+  return res.json();
 }
-var getRestaurants = "";
+
+const URL = 'https://benedikteeva.dk/jwtBackend%2D1.0%2DSNAPSHOT/api/restaurants/name';
+
 class FoursquareFacade {
-
-
-    //TODO: At some point we will use fetch to get data from our rest endpoints but not made yet. 
-    fetchData = async (location, callback) => {
-
-        fetch(URL + location).then(async function (response) {
-            return await response.json();
-        })
-            .then(function (data) {
-
-
-                const restaurantsTbody = data.response.groups[0].items
-            
-               callback(restaurantsTbody)
-            })
+    handleData = (data, callback) => {
+      let restaurants = [];
+      restaurants = data.response.groups[0].items;
+      callback(restaurants);
     }
+
+    fetch = location => // , callback
+
+      fetch(URL + location)
+        .then(async (results) => {
+          if (!results.ok) {
+            throw Error(results.statusText);
+          }
+          return await results.json();
+        })
+        /*  .then(data => {
+             var restaurants = []
+             restaurants = data.response.groups[0].items;
+             callback(restaurants);
+         }) */
 }
-
-
-
-
 
 const fourfacade = new FoursquareFacade();
 
