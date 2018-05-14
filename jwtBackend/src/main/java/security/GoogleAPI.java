8826b82bf46a6849044a4dc5ff29ec7524f0bbf4
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-05-14 15:23:30 +0200

    Merge origin/developer into TesCI
    Conflicts:
    	Frontend/src/SearchGoogle.js
    	jwtBackend/src/main/java/security/GoogleAPI.java

--- jwtBackend/src/main/java/security/GoogleAPI.java
+++ jwtBackend/src/main/java/security/GoogleAPI.java
@@ -40,7 +40,7 @@ public class GoogleAPI {
         return outPut;
 }
     private static final String UrlFirstPart = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
-    private static final String StandardRadius = "&radius=2000";
+    private static final String StandardRadius = "&radius=10000";
 //  private static final String UrlKey = "AIzaSyBbdu5tPAp2P0EGbFgdGfzk_Vz7GUbsNO0"; //Bos
 // private static final String UrlKey = "AIzaSyDr5OFAHNYW2AdC2R8hqd3vH6QzirUkeNg"; // benediktes
  private static final String UrlKey= "AIzaSyDNGntL1NjT4xTfiMxnq2Blu6M5yjfPmMM";
--- jwtBackend/target/generated-sources/annotations/entity/Role_.java
+++ jwtBackend/target/generated-sources/annotations/entity/Role_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-09T11:30:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-12T16:22:47")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
