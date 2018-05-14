aeec1c436a4f44ac37d60cb2e0885cc7d3d55fac
walter01git <26571249+walter01git@users.noreply.github.com>
2018-05-14 10:13:23 +0200

    Added get all users methoed

--- jwtBackend/src/main/java/entity/UserFacade.java
+++ jwtBackend/src/main/java/entity/UserFacade.java
@@ -7,6 +7,10 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import exceptions.AuthenticationException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import javax.persistence.Query;
 
 /**
  *
@@ -57,5 +61,36 @@ public class UserFacade {
         return user;
     }
     
+   public static List< User> getaUserList(EntityManager em) {
 
+       // Query q = em.createQuery("SELECT u.userName FROM User u");
+         Query q = em.createQuery("SELECT u.userName, r.roleName FROM User u, Role r INNER JOIN r.userList, u.roleList");
+        List<User> userList =q.getResultList();
+        System.out.println(userList);
+//        for (int i = 0; i < userList.size(); i++) {
+//         
+//            userList.add( userList.get(i));//persons.put(i, personnes.get(i));
+//        }
+
+        return userList;
+
+    }
+   
+   public static List<Role> getaUserListRole(EntityManager em) {
+
+        Query q = em.createQuery("SELECT r.roleName FROM Role r");
+        List<Role> userListRole =q.getResultList();
+        System.out.println(userListRole);
+
+        return userListRole;
+
+    }
+    
+
+    public static void main(String[] args) {
+          EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+            EntityManager em = emf.createEntityManager();
+        System.out.println(getaUserList(em));
+    }
+   
 }
