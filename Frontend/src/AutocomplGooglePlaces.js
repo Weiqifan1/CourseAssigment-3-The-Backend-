bc4c0bbb6aa201571164fb17cb65d0db49061593
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-05-14 10:26:18 +0200

    working autocomplete

--- Frontend/src/AutocomplGooglePlaces.js
+++ Frontend/src/AutocomplGooglePlaces.js
@@ -28,7 +28,31 @@ export default class LocationSearchInput extends React.Component {
     await GoogleFacade.fetchRestaurantsByQuery(this.state.location);
     GoogleFacade.getRestaurantsByLocation();
     this.setState({ restaurants: GoogleFacade.getRestaurantsByLocation() });
-  }
+    const restaurantArray = this.state.restaurants.map(restaurant =>
+      (
+
+        <div>
+
+          <table className="table">
+            <thead />
+            <tbody>
+              <tr key={restaurant.id}>
+                <td><img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${restaurant.photos[0].photo_reference}&key=AIzaSyDNGntL1NjT4xTfiMxnq2Blu6M5yjfPmMM`} height="75" alt="noimage" /></td>
+                <td><p>{restaurant.name}</p>{restaurant.formatted_address}</td>
+                <td>Rating: {restaurant.rating}</td>
+                <td />
+
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      ),
+    );
+
+    this.setState({ restaurantTable: restaurantArray });
+  };
+
   render() {
     return (
       <form onSubmit={this.onSubmit} >
@@ -61,6 +85,7 @@ export default class LocationSearchInput extends React.Component {
                 );
               })}
               </div>
+              {this.state.restaurantTable}
             </div>
         )}
         </PlacesAutocomplete>
