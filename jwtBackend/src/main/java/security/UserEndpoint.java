d2b56495736e63b94d10442c6caf31ed5e39999c
walter01git <26571249+walter01git@users.noreply.github.com>
2018-05-16 12:43:36 +0200

    Tilføjelse af UserEndpoint Exception til håntering af DB fejl. Metoden er nærmest færdig, mangler bare at catche eller throwe en anden exception.

--- jwtBackend/src/main/java/security/UserEndpoint.java
+++ jwtBackend/src/main/java/security/UserEndpoint.java
@@ -2,14 +2,21 @@ package security;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.mysql.jdbc.CommunicationsException;
 import entity.UserFacade;
+import exceptions.PersistenceException;
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.component.UpdateModelException;
+import javax.persistence.NoResultException;
+import org.eclipse.persistence.exceptions.JPQLException;
 @Path("users")
 
 public class UserEndpoint {
@@ -17,17 +24,27 @@ public class UserEndpoint {
     @GET
     //@Path("/allusers")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getUsers() throws IOException { //@PathParam("place")
+    public static Response getUsers() throws PersistenceException { //@PathParam("place")
+        
+    try {
         String userNames = UserFacade.getInstance().getaUserList().toString();
 
         JsonObject responseJson = new JsonObject();
         responseJson.addProperty("usernames", userNames);
         return Response.ok(new Gson().toJson(responseJson)).build();
+   
+        } catch (Exception ex) {
+          Logger.getLogger(PersistenceException.class.getName()).log(Level.SEVERE, "Fejl i Database forbindelse", ex);
+          System.out.println("Her fremvises Ezceptions besked:  " + ex.getMessage());
+            throw new PersistenceException(ex.getMessage());
+        }
     }
+       
 
-    public static void main(String[] args) throws IOException {
 
-        //System.out.println(getUsers());
+    public static void main(String[] args) throws IOException, PersistenceException {
+
+        System.out.println(getUsers());
     }
 
 }
