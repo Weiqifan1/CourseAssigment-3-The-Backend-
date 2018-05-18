31db7690059fb63a1e53046f04ea89676a6baa1d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-18 11:15:27 +0000

    Deploy Weiqifan1/sem3pro to github.com/Weiqifan1/sem3pro.git:gh-pages

--- jwtBackend/target/generated-sources/annotations/entity/Role_.java
+++ jwtBackend/target/generated-sources/annotations/entity/Role_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-18T10:52:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-18T11:15:11")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
